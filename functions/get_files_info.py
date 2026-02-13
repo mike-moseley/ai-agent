4c180cc1dfe034e2f3290eb68851d060fb3fcb78
Mike Moseley <mike.moseley@tuta.io>
2026-02-13 06:45:22 -0500

    Finished CH2:L2

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,29 +1,32 @@
+from requests.packages import target
 import os
 
 
 def get_files_info(working_directory, directory):
-    if not os.path.isdir(directory):
-        raise Exception(f'Error: "{directory}" is not a directory')
-
     working_directory = os.path.abspath(working_directory)
     target_directory = os.path.normpath(os.path.join(working_directory, directory))
     valid_target_directory = (
         os.path.commonpath([working_directory, target_directory]) == working_directory
     )
+    if directory == ".":
+        result_string = "Result for current directory:\n"
+    else:
+        result_string = f"Result for '{directory}' directory:\n"
 
     if not valid_target_directory:
-        raise Exception(
-            f'Error: cannot list "{directory}" as it is outside the permitted working directory'
-        )
+        return f'{result_string}    Error: cannot list "{directory}" as it is outside the permitted working directory'
+    if not os.path.isdir(target_directory):
+        return f'   Error: "{result_string}{directory}" is not a directory'
 
     directory_list = os.listdir(target_directory)
 
-    result_string = f"Result for {directory}\n"
     for f in directory_list:
+        f_path = os.path.join(target_directory, f)
         try:
             result_string = (
                 result_string
-                + f"  - {f}: {os.path.getsize(f)}, is_dir={os.path.isdir(f)}"
+                + f"  - {f}: {os.path.getsize(f_path)}, is_dir={os.path.isdir(f_path)}\n"
             )
         except Exception as error:
-            return f"Error: {error}"
+            return f"{result_string}    Error: {error}"
+    return result_string
