b3bb07b1c9dd18b295170ad361d5fabccb5b1610
Mike Moseley <mike.moseley@tuta.io>
2026-02-14 12:04:39 -0500

    Course completed

--- main.py
+++ main.py
@@ -25,31 +25,41 @@ def main():
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
     client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=messages,
-        config=types.GenerateContentConfig(tools=[available_functions],system_instruction=system_prompt)
-    )
-    if response.usage_metadata is None:
-        raise RuntimeError("API request failed")
-
-    if args.verbose:
-        print(
-            f"User prompt: {args.user_prompt} \nPrompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}"
+    
+    for _ in range(15):
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=messages,
+            config=types.GenerateContentConfig(tools=[available_functions],system_instruction=system_prompt)
         )
-    function_results = []
-    if response.function_calls is not None:
-        function_call_result = call_function(response.function_calls[0])
+        if response.usage_metadata is None:
+            raise RuntimeError("API request failed")
 
-        if function_call_result.parts[0].function_response is None:
-            raise Exception("  Error: {err}")
-
-        if function_call_result.parts[0].function_response.response is None:
-            raise Exception("  Error: {err}")
+        if response.candidates is not None:
+            for message in response.candidates:
+                messages.append(message.content)
 
         if args.verbose:
-            print(f"-> {function_call_result.parts[0].function_response.response}")
-
+            print(
+                f"User prompt: {args.user_prompt} \nPrompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}"
+            )
+        function_results = []
+        if response.function_calls is not None:
+            for f in response.function_calls:
+                print(f'Calling function "{f.name}" on: {f.args}\n')
+                function_call_result = call_function(f)
+                if function_call_result.parts[0].function_response is None:
+                    raise Exception("  Error: {err}")
+                if function_call_result.parts[0].function_response.response is None:
+                    raise Exception("  Error: {err}")
+                if args.verbose:
+                    print(f"-> {function_call_result.parts[0].function_response.response}")
+                function_results.append(function_call_result.parts[0])
+            messages.append(types.Content(role="user", parts=function_results))
+        else:
+            print(response.text)
+            exit(0)
+    exit(1)
 
 if __name__ == "__main__":
     main()
